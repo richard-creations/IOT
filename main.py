323506672ccac359de2f2b4732212e7c1e666707
Richard M <richardm@Richards-MacBook-Pro-2.local>
2021-04-28 01:05:45 -0500

    added question querying and question syntax check

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import speech_recognition as sr  
 import pyttsx3
-import pprint
+import bs4
+import requests
+
+try:
+    from googlesearch import search
+except ImportError: 
+    print("No module named 'google' found")
+
 engine = pyttsx3.init()
 
 #generate a list of all audio cards/microphones
-mic_name = "MacBook Pro Microphone"
+mic_name = 'MacBook Pro Microphone'
 
 mic_list = sr.Microphone.list_microphone_names()
 print(mic_list)
@@ -22,7 +29,7 @@ print(f"Using mic: {mic_list[device_id]}")
 ##### SPEECH VOICE SET-UP ########
 """ RATE"""
 rate = engine.getProperty('rate')   # getting details of current speaking rate
-engine.setProperty('rate', 160)     # setting up new voice rate
+engine.setProperty('rate', 170)     # setting up new voice rate
 
 
 """VOLUME"""
@@ -51,22 +58,28 @@ deviceList['speaker'] = {'bedroom':{'status': 0 }, 'google home':{'status': 0 },
 
 #################### QUESTION SYNTAX ###################
 question_tags = ('what', 'how', 'who', 'where', 'when', 'is it')
-modifiers = ('many', 'much', 'can' )
-question_follow = ('are', 'is', 'who'  )
+question_follow = ('are', 'is', 'who', 'many', 'much', 'can', 'was', 'old')
 
-def isQuestion():
-    return 0
-
+def isQuestion(text1):
+    text = text1.split()
+    for tag in question_tags: #O(n)
+        try:
+            index = text.index(tag) #O(1)
+            if text[index +1] in question_follow: #O(n)
+                return True
+        except:
+            continue
+    return False
 ##################### END QUESTION TEXT ##############
 
 def deviceSwitch(code, text):
     success = 0
     for device in deviceList:
         if device in text:
-            print(device) 
+           # print(device) 
             for deviceName in deviceList[device]:
                 if deviceName in text:
-                    print(deviceName)
+                   # print(deviceName)
                     deviceList[device][deviceName]['status'] = code
                     success = 1
                     break
@@ -83,8 +96,8 @@ def deviceSwitch(code, text):
             engine.runAndWait()
         print(deviceList)
     else:
-        print("Sorry, device not recognized")
-        engine.say("Sorry, device not recognized")
+        print("Sorry, I don't recognize that device.")
+        engine.say("Sorry, I don't recognize that device.")
         engine.runAndWait()
        
             
@@ -93,16 +106,36 @@ def processText(text):
     if "turn on" in text:
         deviceSwitch(1, text)
 
-    if "turn off" in text:
+    elif "turn off" in text:
         deviceSwitch(0, text)
     
-    if "what is" in text or "how" in text or "when" in text:
-        return 0
+    elif isQuestion(text):
+        print("This is a question I have to search.")
+        url = 'https://google.com/search?q=' + text
+        
+        # Fetch the URL data using requests.get(url),
+        # store it in a variable, request_result.
+        request_result=requests.get( url )
+        
+        # Creating soup from the fetched request
+        soup = bs4.BeautifulSoup(request_result.text,
+                                "html.parser")
+        
+        # soup.find.all( h3 ) to grab 
+        # all major headings of our search result,
+        heading_object=soup.find_all( 'h3' )
+        
+        # Iterate through the object 
+        # and print it as a string.
+        print(heading_object[0].getText())
+        engine.say(heading_object[0].getText())
+        engine.runAndWait()
+        print("------")
+
 
 if __name__ == "__main__":
-    while("shut down" not in text):
-        r = sr.Recognizer()  
-      
+    while("shut down" not in text): 
+        r = sr.Recognizer() 
         with sr.Microphone(device_index = device_id) as source: 
             r.adjust_for_ambient_noise(source)                                                                      
             print("Speak:")                                                                                   
@@ -114,7 +147,6 @@ if __name__ == "__main__":
         
         except sr.UnknownValueError:
             print("Could not understand audio")
-            engine.runAndWait()
         except sr.RequestError as e:
             print("Could not request results; {0}".format(e))
     print("shutting down...")
